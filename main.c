5606a6252b0c49ebf1a0380a011697c3beb19c3c
pxvxax <pxvxax@gmail.com>
2018-10-26 08:35:41 +0200

    liczby naturalne doszły zmiany

--- main.c
+++ main.c
@@ -2,11 +2,23 @@
 
 int main() {
     int a;
+    int licznik_petli = 0;
 
 
     for (a = 100; a >= 1; --a){
+        if (!(a % 2))
 
-printf("Liczby naturalne od 100 do 1 to jest: %d\n", a);
+
+
+printf("Liczby parzyste od 1 do 100 to jest: %d\n", a);
+
+        licznik_petli++;
+
     }
+
+    printf("\n");
+    printf("liczba pętli wyniosła: %d\n", licznik_petli);
+
+
     return 0;
 }
